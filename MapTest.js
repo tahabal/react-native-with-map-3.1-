bdab33d2be87b2e52f3e29511965048edfd76388
Taha Bal <taha-bal@hotmail.com>
2018-05-22 14:37:48 +0300

    added nativeBase

--- MapTest.js
+++ MapTest.js
@@ -5,14 +5,27 @@ class MapTest extends Component {
   render () {
     return (
       <View style={styles.container}>
-      <MapView
-    initialRegion={{
-      latitude: 37.78825,
-      longitude: -122.4324,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    }}
-  />
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+        <MapView.Marker coordinate=
+        {
+          {
+            latitude: 37.78,
+            longitude: -11.43
+          }
+        }
+        title={"Deneme"}
+        description={"Test"}
+        />
+        </MapView>
       </View>
     )   
   }
