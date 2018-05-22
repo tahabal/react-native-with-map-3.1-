7842104fba6fc5f7653b547c6eb2e263e3ea89a8
Taha Bal <taha-bal@hotmail.com>
2018-05-22 14:55:40 +0300

    added react-naviagation

--- MapTest.js
+++ MapTest.js
@@ -1,37 +1,37 @@
-import React, { Component } from 'react'
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
-import { View, StyleSheet } from 'react-native'
+import React, { Component } from 'react';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { View, StyleSheet } from 'react-native';
 class MapTest extends Component {
-  render () {
-    return (
-      <View style={styles.container}>
-        <MapView
-          provider={PROVIDER_GOOGLE}
-          style={styles.map}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
-        >
-        <MapView.Marker coordinate=
-        {
-          {
-            latitude: 37.78,
-            longitude: -11.43
-          }
-        }
-        title={"Deneme"}
-        description={"Test"}
-        />
-        </MapView>
-      </View>
-    )   
-  }
+    render () {
+        return (
+            <View style={styles.container}>
+                <MapView
+                    provider={PROVIDER_GOOGLE}
+                    style={styles.map}
+                    region={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.015,
+                        longitudeDelta: 0.0121,
+                    }}
+                >
+                    <MapView.Marker coordinate=
+                        {
+                            {
+                                latitude: 37.78,
+                                longitude: -11.43
+                            }
+                        }
+                    title={"Deneme"}
+                    description={"Test"}
+                    />
+                </MapView>
+            </View>
+        );   
+    }
 }
 const styles = StyleSheet.create({
-  container: { ... StyleSheet.absoluteFillObject },
-  map: { ...StyleSheet.absoluteFillObject }
-})
-export default MapTest
+    container: { ... StyleSheet.absoluteFillObject },
+    map: { ...StyleSheet.absoluteFillObject }
+});
+export default MapTest;
